867be6e01412b246c51f7f8ba6909dd64ac4d937
ovidiualb <ovidiu.alb@gmail.com>
2021-01-19 18:39:03 +0200

    Add files via upload

---
+++ CookBook/frmMain.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace CookBook
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cookBookDataSet = new CookBook.CookBookDataSet();
+            this.lblRetete = new System.Windows.Forms.Label();
+            this.lstRetete = new System.Windows.Forms.ListBox();
+            this.lstIngrediente = new System.Windows.Forms.ListBox();
+            this.lblIngredient = new System.Windows.Forms.Label();
+            this.btnAddReteta = new System.Windows.Forms.Button();
+            this.txtNumeReteta = new System.Windows.Forms.TextBox();
+            this.btnUpdateNumeReteta = new System.Windows.Forms.Button();
+            this.lstToateIngredientele = new System.Windows.Forms.ListBox();
+            this.lblToateIngredientele = new System.Windows.Forms.Label();
+            this.btnAdaugaInReteta = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cookBookDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cookBookDataSet
+            // 
+            this.cookBookDataSet.DataSetName = "CookBookDataSet";
+            this.cookBookDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // lblRetete
+            // 
+            this.lblRetete.AutoSize = true;
+            this.lblRetete.Location = new System.Drawing.Point(34, 100);
+            this.lblRetete.Name = "lblRetete";
+            this.lblRetete.Size = new System.Drawing.Size(39, 13);
+            this.lblRetete.TabIndex = 2;
+            this.lblRetete.Text = "Retete";
+            // 
+            // lstRetete
+            // 
+            this.lstRetete.FormattingEnabled = true;
+            this.lstRetete.Location = new System.Drawing.Point(37, 116);
+            this.lstRetete.Name = "lstRetete";
+            this.lstRetete.Size = new System.Drawing.Size(151, 277);
+            this.lstRetete.TabIndex = 3;
+            this.lstRetete.SelectedIndexChanged += new System.EventHandler(this.lstRetete_SelectedIndexChanged);
+            // 
+            // lstIngrediente
+            // 
+            this.lstIngrediente.FormattingEnabled = true;
+            this.lstIngrediente.Location = new System.Drawing.Point(37, 429);
+            this.lstIngrediente.Name = "lstIngrediente";
+            this.lstIngrediente.Size = new System.Drawing.Size(151, 251);
+            this.lstIngrediente.TabIndex = 5;
+            // 
+            // lblIngredient
+            // 
+            this.lblIngredient.AutoSize = true;
+            this.lblIngredient.Location = new System.Drawing.Point(34, 413);
+            this.lblIngredient.Name = "lblIngredient";
+            this.lblIngredient.Size = new System.Drawing.Size(95, 13);
+            this.lblIngredient.TabIndex = 4;
+            this.lblIngredient.Text = "Ingrediente Reteta";
+            // 
+            // btnAddReteta
+            // 
+            this.btnAddReteta.Location = new System.Drawing.Point(37, 39);
+            this.btnAddReteta.Name = "btnAddReteta";
+            this.btnAddReteta.Size = new System.Drawing.Size(151, 23);
+            this.btnAddReteta.TabIndex = 6;
+            this.btnAddReteta.Text = "Reteta Noua";
+            this.btnAddReteta.UseVisualStyleBackColor = true;
+            // 
+            // txtNumeReteta
+            // 
+            this.txtNumeReteta.Location = new System.Drawing.Point(37, 13);
+            this.txtNumeReteta.Name = "txtNumeReteta";
+            this.txtNumeReteta.Size = new System.Drawing.Size(151, 20);
+            this.txtNumeReteta.TabIndex = 7;
+            // 
+            // btnUpdateNumeReteta
+            // 
+            this.btnUpdateNumeReteta.Location = new System.Drawing.Point(37, 68);
+            this.btnUpdateNumeReteta.Name = "btnUpdateNumeReteta";
+            this.btnUpdateNumeReteta.Size = new System.Drawing.Size(151, 23);
+            this.btnUpdateNumeReteta.TabIndex = 8;
+            this.btnUpdateNumeReteta.Text = "Actualizeaza Nume Reteta";
+            this.btnUpdateNumeReteta.UseVisualStyleBackColor = true;
+            // 
+            // lstToateIngredientele
+            // 
+            this.lstToateIngredientele.FormattingEnabled = true;
+            this.lstToateIngredientele.Location = new System.Drawing.Point(254, 116);
+            this.lstToateIngredientele.Name = "lstToateIngredientele";
+            this.lstToateIngredientele.Size = new System.Drawing.Size(151, 277);
+            this.lstToateIngredientele.TabIndex = 10;
+            // 
+            // lblToateIngredientele
+            // 
+            this.lblToateIngredientele.AutoSize = true;
+            this.lblToateIngredientele.Location = new System.Drawing.Point(251, 100);
+            this.lblToateIngredientele.Name = "lblToateIngredientele";
+            this.lblToateIngredientele.Size = new System.Drawing.Size(99, 13);
+            this.lblToateIngredientele.TabIndex = 9;
+            this.lblToateIngredientele.Text = "Toate Ingredientele";
+            // 
+            // btnAdaugaInReteta
+            // 
+            this.btnAdaugaInReteta.Location = new System.Drawing.Point(254, 399);
+            this.btnAdaugaInReteta.Name = "btnAdaugaInReteta";
+            this.btnAdaugaInReteta.Size = new System.Drawing.Size(151, 23);
+            this.btnAdaugaInReteta.TabIndex = 11;
+            this.btnAdaugaInReteta.Text = "Adauga in reteta";
+            this.btnAdaugaInReteta.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 729);
+            this.Controls.Add(this.btnAdaugaInReteta);
+            this.Controls.Add(this.lstToateIngredientele);
+            this.Controls.Add(this.lblToateIngredientele);
+            this.Controls.Add(this.btnUpdateNumeReteta);
+            this.Controls.Add(this.txtNumeReteta);
+            this.Controls.Add(this.btnAddReteta);
+            this.Controls.Add(this.lstIngrediente);
+            this.Controls.Add(this.lblIngredient);
+            this.Controls.Add(this.lstRetete);
+            this.Controls.Add(this.lblRetete);
+            this.Name = "frmMain";
+            this.Text = "CookBook";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cookBookDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CookBookDataSet cookBookDataSet;
+        private System.Windows.Forms.Label lblRetete;
+        private System.Windows.Forms.ListBox lstRetete;
+        private System.Windows.Forms.ListBox lstIngrediente;
+        private System.Windows.Forms.Label lblIngredient;
+        private System.Windows.Forms.Button btnAddReteta;
+        private System.Windows.Forms.TextBox txtNumeReteta;
+        private System.Windows.Forms.Button btnUpdateNumeReteta;
+        private System.Windows.Forms.ListBox lstToateIngredientele;
+        private System.Windows.Forms.Label lblToateIngredientele;
+        private System.Windows.Forms.Button btnAdaugaInReteta;
+    }
+}
+
---
+++ CookBook/frmMain.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CookBook
+{
+    public partial class frmMain : Form
+    {
+        SqlConnection connection;
+        string connectionString;
+        
+        public frmMain()
+        {
+            InitializeComponent();
+            btnAddReteta.Click +=btnAddReteta_Click;
+            btnUpdateNumeReteta.Click +=btnUpdateNumeReteta_Click;
+            btnAdaugaInReteta.Click +=btnAdaugaInReteta_Click;
+            connectionString = ConfigurationManager.ConnectionStrings["CookBook.Properties.Settings.CookBookConnectionString"].ConnectionString;
+        }
+
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            PopulateRetete();
+            PopulateToateIngredientele();
+        }
+
+        private void PopulateRetete()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using(SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Reteta", connection))
+            {
+           
+                DataTable retetaTable = new DataTable();
+                adapter.Fill(retetaTable);
+
+                lstRetete.DisplayMember = "Nume";
+                lstRetete.ValueMember = "Id";
+                lstRetete.DataSource = retetaTable;
+            }
+        }
+
+
+        private void PopulateToateIngredientele()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Ingredient", connection))
+            {
+
+                DataTable ingredientTable = new DataTable();
+                adapter.Fill(ingredientTable);
+
+                lstToateIngredientele.DisplayMember = "Nume";
+                lstToateIngredientele.ValueMember = "Id";
+                lstToateIngredientele.DataSource = ingredientTable;
+            }
+        }
+
+
+        private void PopulateIngrediente()
+        {
+            string query = "SELECT a.Nume FROM Ingredient a " +
+                "INNER JOIN RetetaIngredient b ON a.Id = b.IngredientId " +
+                "WHERE b.RetetaId = @ReteteId";
+            
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@ReteteId", lstRetete.SelectedValue);
+                
+                DataTable ingredientTable = new DataTable();
+                adapter.Fill(ingredientTable);
+
+                lstIngrediente.DisplayMember = "Nume";
+                lstIngrediente.ValueMember = "Id";
+                lstIngrediente.DataSource = ingredientTable;
+            }
+        }
+
+
+        private void lstRetete_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateIngrediente();
+        }
+
+        private void btnAddReteta_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO Reteta VALUES (@NumeReteta, 80, 'Mix toate, pe rand')";
+            
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                
+                command.Parameters.AddWithValue("@NumeReteta", txtNumeReteta.Text);
+
+                command.ExecuteNonQuery();
+            }
+
+            PopulateRetete();
+
+        }
+
+        private void btnUpdateNumeReteta_Click(object sender, EventArgs e)
+        {
+         string query = "UPDATE Reteta SET Nume = @NumeReteta WHERE Id = @ReteteId";
+            
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                
+                command.Parameters.AddWithValue("@NumeReteta", txtNumeReteta.Text);
+                command.Parameters.AddWithValue ("@ReteteId", lstRetete.SelectedValue);
+
+                command.ExecuteNonQuery();
+            }
+
+            PopulateRetete();
+        }
+
+        private void btnAdaugaInReteta_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO RetetaIngredient VALUES (@ReteteId, @IngredientId)";
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                command.Parameters.AddWithValue("@ReteteId", lstRetete.SelectedValue);
+                command.Parameters.AddWithValue("@IngredientId", lstToateIngredientele.SelectedValue);
+
+                command.ExecuteNonQuery();
+            }
+
+            PopulateRetete();
+
+        }
+    }
+}
